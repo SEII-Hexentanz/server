3e21e124772eee0dc64b8b32053a6623e80fd3a9
TheDome0 <dostoeby@edu.aau.at>
2024-03-28 16:58:53 +0100

    update build.gradle
    add sonar & jacoco

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     java
+    jacoco
+    alias(libs.plugins.sonarqube)
 }
 
 group = "at.aau"
@@ -11,6 +13,7 @@ repositories {
 
 dependencies {
     testImplementation(libs.junit)
+    testRuntimeOnly(libs.junitEngine)
     implementation(libs.vavr)
 }
 
@@ -21,4 +24,22 @@ java {
 
 tasks.test {
     useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test) // tests are required to run before generating the report
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "SEII-Hexentanz_server")
+        property("sonar.organization", "seii-hexentanz")
+        property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.java.coveragePlugin", "jacoco")
+        property(
+            "sonar.coverage.jacoco.xmlReportPaths",
+            "${project.projectDir}/build/reports/jacoco/jacocoTestReport/jacocoTestReport.xml"
+        )
+    }
 }
