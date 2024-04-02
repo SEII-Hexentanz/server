845b0f71d51a4e9e12f0eecdcb66a265ca47e808
TheDome0 <dostoeby@edu.aau.at>
2024-04-02 14:14:32 +0200

    fix color logic

--- src/main/java/at/aau/commands/RegisterCommand.java
+++ src/main/java/at/aau/commands/RegisterCommand.java
@@ -4,19 +4,24 @@ import at.aau.Game;
 import at.aau.commandHandler.Command;
 import at.aau.models.Player;
 import at.aau.models.Response;
-import at.aau.payloads.EmptyPayload;
 import at.aau.payloads.Payload;
+import at.aau.values.Color;
 import at.aau.values.GameState;
 import at.aau.values.ResponseType;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class RegisterCommand implements Command {
     @Override
     public void execute(Game game, Player player, Payload payload) {
-        if (game.players.size() <= 6 || game.state != GameState.LOBBY) {
-            player.send(new Response(ResponseType.GAME_FULL, new EmptyPayload()));
+        if (game.getPlayers().size() >= 6 || game.getState() != GameState.LOBBY) {
+            player.send(new Response(ResponseType.GAME_FULL));
         } else {
-            player.color = game.players.stream().map(Player::color).reduce((c1, c2) -> c1 == c2 ? c1 : null).orElse(null);
-            game.players.add(player);
+            var usedColors = game.getPlayers().stream().map(Player::color).collect(Collectors.toSet());
+            var color = Arrays.stream(Color.values()).filter(c -> !usedColors.contains(c)).findFirst().orElseThrow();
+            player.setColor(color);
+            game.getPlayers().add(player);
         }
     }
 }
