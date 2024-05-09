773987eac31363aae3c23f71e9846dc9bfccff07
melly0102 <mebostjancic@edu.aau.at>
2024-05-09 17:33:18 +0200

    KT: create Dice class on server side

---
+++ src/main/java/at/aau/Dice.java
@@ -0,0 +1,15 @@
+package at.aau;
+
+import java.security.SecureRandom;
+
+public class Dice {
+    private int diceNumber;
+
+    public Dice(int diceNumber){
+        this.diceNumber = diceNumber;
+    }
+    public int getDiceNumber() {
+        return diceNumber;
+    }
+
+}
