aeb4c190f25b2ac194521939b4a9156e9a0e57dc
TheDome0 <dostoeby@edu.aau.at>
2024-05-15 20:58:27 +0200

    fix sonar

--- src/main/java/at/aau/Dice.java
+++ src/main/java/at/aau/Dice.java
@@ -3,36 +3,14 @@ package at.aau;
 import java.security.SecureRandom;
 
 public class Dice {
-    private int diceNumber;
-    private SecureRandom ran;
+    private final SecureRandom ran;
     // as sonarcloud suggests due to security reason use security random instead of ran
 
     public Dice() {
-        this.diceNumber = diceNumber;
         ran = new SecureRandom();
     }
 
-    public int getDiceNumber() {
-        return diceNumber;
-
-    }
-
     public int useDice() {
-        return diceNumber = ran.nextInt(6) + 1;
+        return ran.nextInt(6) + 1;
     }
-
-    //is also sum of dice
-    public int getDice() {
-        return diceNumber;
-    }
-
-
-    public void setDice(int diceNumber) {
-        this.diceNumber = diceNumber;
-    }
-
-    public void setRan(SecureRandom ran) {
-        this.ran = ran;
-    }
-
 }
