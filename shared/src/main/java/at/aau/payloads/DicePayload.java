5b15254228a1f02d4bea48bbb68d4b3482bc0dcf
melly0102 <mebostjancic@edu.aau.at>
2024-05-15 22:05:56 +0200

    fix: dicePayload and commandhandler

--- shared/src/main/java/at/aau/payloads/DicePayload.java
+++ shared/src/main/java/at/aau/payloads/DicePayload.java
@@ -2,7 +2,8 @@ package at.aau.payloads;
 
 import java.io.Serial;
 
-public record DicePayload(int diceValue) implements Payload{
+public record DicePayload(int diceValue) implements Payload {
+
     @Serial
-    private static final long serialVersionUID = 1383217793982015942L;
+    private static final long serialVersionUID= 1921381893121L;
 }
