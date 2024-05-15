945b569e24099f1a0d6fa5f41c5687e77321e3d6
TheDome0 <dostoeby@edu.aau.at>
2024-05-15 22:31:58 +0200

    revert changes
    pretty sure not all commands will be a move & dice roll at the same time so this doesn't make any sense

--- src/main/java/at/aau/commandHandler/CommandHandler.java
+++ src/main/java/at/aau/commandHandler/CommandHandler.java
@@ -2,14 +2,12 @@ package at.aau.commandHandler;
 
 import at.aau.Game;
 import at.aau.Player;
-import at.aau.commands.DiceCommand;
 import at.aau.commands.PingCommand;
 import at.aau.commands.PlayerMoveCommand;
 import at.aau.commands.RegisterCommand;
 import at.aau.commands.StartCommand;
 import at.aau.models.Request;
 import at.aau.models.Response;
-import at.aau.payloads.PlayerMovePayload;
 import at.aau.payloads.UpdateStatePayload;
 import at.aau.values.CommandType;
 import at.aau.values.ResponseType;
@@ -22,7 +20,7 @@ public class CommandHandler {
         put(CommandType.PING, new PingCommand());
         put(CommandType.REGISTER, new RegisterCommand());
         put(CommandType.CHEAT, new RegisterCommand());
-        put(CommandType.DICE_ROLL, new DiceCommand());
+        put(CommandType.DICE_ROLL, new PingCommand());
         put(CommandType.PLAYER_MOVE, new PlayerMoveCommand());
         put(CommandType.START, new StartCommand());
     }};
@@ -30,7 +28,5 @@ public class CommandHandler {
     public static void execute(Request request, Player player, Game game) {
         commands.get(request.commandType()).execute(game, player, request.payload());
         game.broadcast(new Response(ResponseType.UPDATE_STATE, new UpdateStatePayload(game.toModel())));
-        game.broadcast(new Response(ResponseType.MOVE_SUCCESSFUL));
-        game.broadcast(new Response(ResponseType.DICE_THROWN));
     }
 }
