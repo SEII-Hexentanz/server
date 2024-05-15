a424c7b50f423afb29c6c89bf362083554f83f12
Fipsi <phigattenloehner@outlook.com>
2024-05-15 11:49:22 +0200

    add methods for broadcasting

--- shared/src/main/java/at/aau/payloads/PlayerMovePayload.java
+++ shared/src/main/java/at/aau/payloads/PlayerMovePayload.java
@@ -2,7 +2,7 @@ package at.aau.payloads;
 
 import java.io.Serial;
 
-public record PlayerMovePayload(int oldPosition,int newPosition) implements Payload {
+public record PlayerMovePayload(int oldPosition,int newPosition, String playerName) implements Payload {
     @Serial
     private static final long serialVersionUID= -1783123104231656319L;
 }
