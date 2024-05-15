146de55f24026e0b127b33c54408eee34cc7afcc
Fipsi <phigattenloehner@outlook.com>
2024-05-15 19:46:45 +0200

    remove responsetype movesuccessful

--- shared/src/main/java/at/aau/payloads/MoveSuccessfulPayload.java
+++
@@ -1,8 +0,0 @@
-package at.aau.payloads;
-
-import java.io.Serial;
-
-public record MoveSuccessfulPayload(String playerName, int characterIndex, int oldPosition, int newPosition) implements Payload {
-    @Serial
-    private static final long serialVersionUID= 3450535204952368681L;
-}
