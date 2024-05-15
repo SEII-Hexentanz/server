906b87135950d1011811067a581a212335c8734f
Fipsi <phigattenloehner@outlook.com>
2024-05-15 03:08:09 +0200

    add new payloads, cmds

--- shared/src/main/java/at/aau/payloads/PlayerMovePayload.java
+++ shared/src/main/java/at/aau/payloads/PlayerMovePayload.java
@@ -3,6 +3,6 @@ package at.aau.payloads;
 import java.io.Serial;
 
 public record PlayerMovePayload(int newPosition) implements Payload {
-
+    @Serial
     private static final long serialVersionUID= -1783123104231656319L;
 }
