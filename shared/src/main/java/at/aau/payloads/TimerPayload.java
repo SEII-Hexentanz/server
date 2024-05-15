07192e5637fcb4a46fea56554b9c2572c7fab342
TheDome0 <dostoeby@edu.aau.at>
2024-05-15 22:28:26 +0200

    remove Timer stuff
    doesn't belong here and is blank anyway

--- shared/src/main/java/at/aau/payloads/TimerPayload.java
+++
@@ -1,8 +0,0 @@
-package at.aau.payloads;
-
-import java.io.Serial;
-
-public record TimerPayload() implements Payload{
-    @Serial
-    private static final long serialVersionUID= 8828366451294774125L;
-}
