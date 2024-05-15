8cf934008ee66183d8ae50a24d61f8265c9303da
TheDome0 <dostoeby@edu.aau.at>
2024-05-15 20:46:26 +0200

    remove unnecessary Payload & Serializable impls

--- src/main/java/at/aau/Dice.java
+++ src/main/java/at/aau/Dice.java
@@ -1,28 +1,21 @@
 package at.aau;
 
-import java.io.Serializable;
 import java.security.SecureRandom;
-import java.util.Locale;
 
-import at.aau.payloads.Payload;
-
-public class Dice implements Payload, Serializable {
+public class Dice {
     private int diceNumber;
     private SecureRandom ran;
     // as sonarcloud suggests due to security reason use security random instead of ran
 
-
-
-    public Dice(){
+    public Dice() {
         this.diceNumber = diceNumber;
         ran = new SecureRandom();
     }
+
     public int getDiceNumber() {
         return diceNumber;
 
     }
-
-
 
     public int useDice() {
         return diceNumber = ran.nextInt(6) + 1;
@@ -34,13 +27,12 @@ public class Dice implements Payload, Serializable {
     }
 
 
-    public void setDice(int diceNumber){this.diceNumber=diceNumber;}
+    public void setDice(int diceNumber) {
+        this.diceNumber = diceNumber;
+    }
 
     public void setRan(SecureRandom ran) {
         this.ran = ran;
     }
-
-
-
 
 }
