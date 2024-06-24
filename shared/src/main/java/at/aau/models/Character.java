86ef9bca464addfddad5f901557cedd7ec313e15
Fipsi <phigattenloehner@outlook.com>
2024-06-24 16:05:07 +0200

    remove unnede methods

--- shared/src/main/java/at/aau/models/Character.java
+++ shared/src/main/java/at/aau/models/Character.java
@@ -10,17 +10,4 @@ public record Character(UUID id, int position, CharacterState status, int steps)
     @Serial
     private static final long serialVersionUID = -8185411860134618456L;
 
-    // Method to return a new Character with updated position
-    public Character withPosition(int newPosition) {
-        return new Character(this.id, newPosition, this.status, this.steps);
-    }
-
-    // Optional: Add similar methods for other fields if needed
-    public Character withStatus(CharacterState newStatus) {
-        return new Character(this.id, this.position, newStatus, this.steps);
-    }
-
-    public Character withSteps(int newSteps) {
-        return new Character(this.id, this.position, this.status, newSteps);
-    }
 }
